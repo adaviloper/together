b66781f187b69fea3a0a8d47e8703fbbcb70af70
adaviloper <16654653+adaviloper@users.noreply.github.com>
2026-04-25 19:06:20 -0500

    adding user_id when creating transactions

--- app/Filament/Resources/Transactions/Pages/CreateTransaction.php
+++ app/Filament/Resources/Transactions/Pages/CreateTransaction.php
@@ -4,8 +4,16 @@ namespace App\Filament\Resources\Transactions\Pages;
 
 use App\Filament\Resources\Transactions\TransactionResource;
 use Filament\Resources\Pages\CreateRecord;
+use Illuminate\Support\Facades\Auth;
 
 class CreateTransaction extends CreateRecord
 {
     protected static string $resource = TransactionResource::class;
+
+    protected function mutateFormDataBeforeCreate(array $data): array
+    {
+        $data['user_id'] = Auth::id();
+
+        return $data;
+    }
 }
