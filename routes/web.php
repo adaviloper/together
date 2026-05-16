ba0a4ba8d677276b9e4cbd771d19d442d13da796
adaviloper <16654653+adaviloper@users.noreply.github.com>
2026-05-16 17:09:41 -0500

    redirecting home page to admin panel

--- routes/web.php
+++ routes/web.php
@@ -3,12 +3,9 @@
 use App\Http\Controllers\OrganizationSwitcherController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
-use Laravel\Fortify\Features;
 
 Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canRegister' => Features::enabled(Features::registration()),
-    ]);
+    return redirect('/admin');
 })->name('home');
 
 Route::get('dashboard', function () {
